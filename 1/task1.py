4c552e7b9e255b953e9dc2a85f18958a608ba6ba
d.malkov <d.malkov>
2026-09-15 12:04:06 +0300

    lesson 1 commit

---
+++ 1/task1.py
@@ -0,0 +1,17 @@
+# todo: Определить в коде переменные:
+# 1. Целочисленного типа
+a = 10
+# 2. Вещественного типа
+b = 3.14
+# 3. Логического типа
+flag = True
+# 4. Строкового типа
+text = "stroka"
+# 5. Пустого типа
+unknown = None
+# Вывести их типы.
+print(type(a))
+print(type(b))
+print(type(flag))
+print(type(text))
+print(type(unknown))
---
+++ 1/task2.py
@@ -0,0 +1,25 @@
+# todo: Преобразуйте переменную age и foo в число
+age = "23"
+age = int(age)
+foo = "23abc"
+foo = int(foo)
+#
+# Преобразуйте переменную age в Boolean
+age = "123abc"
+age = bool(age)
+#
+# Преобразуйте переменную flag в Boolean
+flag = 1
+flag = bool(flag)
+#
+# Преобразуйте значение в Boolean
+str_one = "Privet"
+str_one = bool(str_one)
+str_two = ""
+str_two = bool(str_two)
+#
+# Преобразуйте значение 0 и 1 в Boolean
+bool(0)
+bool(1)
+# Преобразуйте False в строку
+str(False)
